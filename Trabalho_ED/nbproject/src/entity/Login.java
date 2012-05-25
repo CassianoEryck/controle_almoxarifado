ffb40fc9703066642d6d7ab30758eb5b5e69459f
Christopher Augusto <christopheeer@gmail.com>
2012-05-25 16:20:04 -0300

    adicionando classe Permissao e ControlLogin

--- Trabalho_ED/nbproject/src/entity/Login.java
+++ Trabalho_ED/nbproject/src/entity/Login.java
@@ -1,12 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package entity;
 
-/**
- *
- * @author Christopher
- */
 public class Login {
     
+    private String usuario;
+    private String senha;
+    private Permissao permissao;
+    
+       
 }
---
+++ Trabalho_ED/nbproject/src/entity/Permissao.java
@@ -0,0 +1,7 @@
+package entity; 
+
+public enum Permissao {
+
+    COMUM, DIRETOR;
+    
+}
